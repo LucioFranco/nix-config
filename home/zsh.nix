689c43f2a712a0af5d36d5ddc58df866574e8f03
Lucio Franco <luciofranco14@gmail.com>
2023-03-23 10:33:39 -0400

    Add venv support

--- home/zsh.nix
+++ home/zsh.nix
@@ -8,29 +8,21 @@
       bbb = "brazil-build-recursive";
     };
 
-    # oh-my-zsh = {
-    #     enable = true;
-    #     plugins = ["git"];
-    # };
+    initExtra = ''
+      # If there is a .venv folder (say created from poetry) in the folder that
+      # we just `cd` into then activate it, once we exit deactivate it.
+      # Ref: https://dev.to/moniquelive/auto-activate-and-deactivate-python-venv-using-zsh-4dlm
+      python_venv() {
+        MYVENV=.venv
+        # when you cd into a folder that contains $MYVENV
+        [[ -d $MYVENV ]] && source $MYVENV/bin/activate > /dev/null 2>&1
+        # when you cd into a folder that doesn't
+        [[ ! -d $MYVENV ]] && deactivate > /dev/null 2>&1
+      }
+      autoload -U add-zsh-hook
+      add-zsh-hook chpwd python_venv
 
-    # initExtra = ''
-    #     export PATH="$PATH:$HOME/.toolbox/bin:$HOME/.cargo/bin"
-
-    #     ${pkgs.any-nix-shell}/bin/any-nix-shell zsh | source /dev/stdin
-    # '';
-
-    # plugins = [
-    #     {
-    #         # https://github.com/softmoth/zsh-vim-mode
-    #         name = "zsh-vim-mode";
-    #         file = "zsh-vim-mode.plugin.zsh";
-    #         src = pkgs.fetchFromGitHub {
-    #             owner = "softmoth";
-    #             repo = "zsh-vim-mode";
-    #             rev = "abef0c0c03506009b56bb94260f846163c4f287a";
-    #             sha256 = "0cnjazclz1kyi13m078ca2v6l8pg4y8jjrry6mkvszd383dx1wib";
-    #         };
-    #     }
-    # ];
+      python_venv
+    '';
   };
 }
