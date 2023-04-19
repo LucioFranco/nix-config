080030456d92f603f2671507a5a2525daf8b68b3
Lucio Franco <luciofranco14@gmail.com>
2023-04-19 11:54:05 -0400

    Add github actions/checks/devshell/packages

--- flake.nix
+++ flake.nix
@@ -13,21 +13,64 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.flake-compat.follows = "flake-compat";
+    };
+
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, darwin, nixpkgs, home-manager }@inputs: {
-    darwinConfigurations.workbook = darwin.lib.darwinSystem rec {
-      system = "aarch64-darwin";
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
+  outputs = { self, darwin, nixpkgs, home-manager, ... }@inputs:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs [ "aarch64-darwin" "x86_64-linux" ];
+    in {
+      hosts = {
+        workbook = {
+          type = "darwin";
+          hostPlatform = "aarch64-darwin";
+        };
+
+        wsl = {
+          type = "homeManager";
+          hostPlatform = "x86_64-linux";
+        };
       };
-      modules = [ ./nix/darwin.nix home-manager.darwinModules.home-manager ];
-    };
 
-    homeConfigurations.wsl = home-manager.lib.homeManagerConfiguration rec {
-      pkgs = nixpkgs.legacyPackages."x86_64-linux";
-      modules = [ ./nix/wsl.nix ];
+      darwinConfigurations.workbook = darwin.lib.darwinSystem rec {
+        system = "aarch64-darwin";
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+        modules = [ ./nix/darwin.nix home-manager.darwinModules.home-manager ];
+      };
+
+      homeConfigurations.wsl = home-manager.lib.homeManagerConfiguration rec {
+        pkgs = nixpkgs.legacyPackages."x86_64-linux";
+        modules = [ ./nix/wsl.nix ];
+      };
+
+      nixosConfigurations = { };
+
+      pkgs = forAllSystems (localSystem:
+        import nixpkgs {
+          inherit localSystem;
+          # overlays = [ self.overlays.default ];
+          config.allowUnfree = true;
+          config.allowAliases = true;
+        });
+
+      checks = forAllSystems (import ./nix/checks.nix inputs);
+      devShells = forAllSystems (import ./nix/dev-shell.nix inputs);
+      packages = forAllSystems (import ./nix/packages.nix inputs);
     };
-  };
 }
