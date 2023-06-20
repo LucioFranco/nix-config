c446ab9b4949c15a56597c060b9070cc290d8e13
Lucio Franco <luciofranco14@gmail.com>
2023-06-19 23:00:29 -0400

    wip

--- nix/nixos.nix
+++ nix/nixos.nix
@@ -1,3 +1,63 @@
 { pkgs, ... }: {
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  fonts.fonts = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk
+    noto-fonts-emoji
+    fira-code
+    fira-code-symbols
+  ];
+
+  home-manager.users.lucio = { ... }: {
+    home.username = "lucio";
+    home.homeDirectory = "/home/lucio";
+
+    home.stateVersion = "23.11";
+    imports = [ ../home ];
+  };
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.trusted-users = [ "lucio" ];
+
+  boot.growPartition = true;
+  boot.loader.grub.device = "/dev/sda";
+
+  swapDevices = [{
+    device = "/var/swap";
+    size = 2048;
+  }];
+
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-label/nixos";
+      autoResize = true;
+      fsType = "ext4";
+    };
+  };
+
+  virtualisation.virtualbox.guest.enable = true;
+
+  boot.loader.grub.fsIdentifier = "provided";
+
+  users.user.lucio = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" ];
+    uid = 1001;
+    password = "demo";
+  };
+
+  services.xserver = {
+    enable = true;
+    desktopManager = {
+      gdm.enable = true;
+      xtermenable = false;
+      defaultSession = "none+i3";
+    };
+
+    windowManager.i3 = {
+      enable = true;
+      extraPackages = with pkgs; [ dmenu i3status i3lock ];
+    };
+  };
 }
