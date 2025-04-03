1224023f270512fdd1e8e3a6a05b6b4b2e801291
Lucio Franco <luciofranco14@gmail.com>
2025-04-03 15:21:55 -0400

    add cargo bin path

--- home/default.nix
+++ home/default.nix
@@ -63,6 +63,7 @@
     "${config.home.homeDirectory}/.npm_global/bin"
     "${config.home.homeDirectory}/go/bin"
     "${config.home.homeDirectory}/.local/bin"
+    "${config.home.homeDirectory}/.cargo/bin"
   ];
 
   # Disable hm's darwin linking logic that conflicts with the custom
