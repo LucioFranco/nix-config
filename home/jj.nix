b258fe3033e22acd5fc0d3f8ea4b9d0173bfea0d
Lucio Franco <luciofranco14@gmail.com>
2025-05-02 10:20:37 -0400

    add jj-diffconflicts

--- home/jj.nix
+++ home/jj.nix
@@ -7,6 +7,26 @@
         name = "Lucio Franco";
         email = "luciofranco14@gmail.com";
       };
+
+      git = {
+        push-bookmark-prefix = "lucio/push-";
+        private-commits = "description(glob:'wip:*') | description(glob:'private:*')";
+      };
+
+      merge-tools.diffconflicts = {
+        program = "nvim";
+        merge-args = [
+          "-c"
+          "let g:jj_diffconflicts_marker_length=$marker_length"
+          "-c"
+          "JJDiffConflicts!"
+          "$output"
+          "$base"
+          "$left"
+          "$right"
+        ];
+        merge-tool-edits-conflict-markers = true;
+      };
     };
   };
 }
