26373619b7d7b1927adbc1c01acefc2331b3a12d
Lucio Franco <luciofranco14@gmail.com>
2025-05-05 16:24:42 -0400

    disable atuin key file secrets

--- home/atuin/default.nix
+++ home/atuin/default.nix
@@ -1,12 +1,13 @@
-{ config, ... }:
+{ ... }:
 {
   age.secrets.key.file = ./key.age;
 
   programs.atuin = {
     enable = true;
     enableZshIntegration = true;
-    settings = {
-      key_path = config.age.secrets.key.path;
-    };
+    # TODO: figure out how to configure the file to read my own secert
+    # settings = {
+    #   key_path = config.age.secrets.key.path;
+    # };
   };
 }
