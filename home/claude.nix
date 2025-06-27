cfa7d2112b3b9415280f0ff39453a8994b5f5b0b
Lucio Franco <luciofranco14@gmail.com>
2025-06-27 11:45:15 -0400

    fmt

--- home/claude.nix
+++ home/claude.nix
@@ -3,21 +3,21 @@
 
   home.packages = [ pkgs.claude-code ];
   # programs.claude-code = {
-    # enable = true;
-    # mcp = {
-    #   filesystem = {
-    #     enable = true;
-    #     allowedPaths = [ "${config.home.homeDirectory}/code" ];
-    #   };
-    #   github = {
-    #     enable = false;
-    #   };
-    #   fetch.enable = false;
-    #   # lsp-rust.enable = true;
-    #   # lsp-python.enable = true;
-    #   # lsp-nix.enable = true;
-    #   sequential-thinking.enable = true;
-    #   # time.enable = true;
-    # };
+  # enable = true;
+  # mcp = {
+  #   filesystem = {
+  #     enable = true;
+  #     allowedPaths = [ "${config.home.homeDirectory}/code" ];
+  #   };
+  #   github = {
+  #     enable = false;
+  #   };
+  #   fetch.enable = false;
+  #   # lsp-rust.enable = true;
+  #   # lsp-python.enable = true;
+  #   # lsp-nix.enable = true;
+  #   sequential-thinking.enable = true;
+  #   # time.enable = true;
+  # };
   # };
 }
